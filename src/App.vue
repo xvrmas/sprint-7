01ab3cff2e821fd1a255272b34a37da112f122ad
Xavier Mas <xvrmasgarcia@gmail.com>
2022-08-17 22:31:52 +0200

    exercici-1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/"></router-link> 
+      <router-link to="/about"></router-link> 
     </nav>
     <router-view/>
   </div>
@@ -13,7 +13,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align:start;
   color: #2c3e50;
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="home">
-    <h1>Home page</h1>
+  <div class="home ml-5">
+    <h4>¿Que quieres hacer?</h4>
+    <div class="m-3">
+      <b-form-checkbox-group
+        stacked
+        v-model="selected"
+        :options="options"
+        value-field="preu"
+        text-field="name"
+      ></b-form-checkbox-group>
+      <h4 class="m-3">Preu: {{ sumaPresupost }} €</h4>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      selected: [],
+      options: [
+        { preu: 500, name: "Una pàgina web (500 €)" },
+        { preu: 300, name: "Una consultoria SEO (300€)" },
+        { preu: 200, name: "Una campanya de Google Ads (200 €)" },
+      ],
+      total: 0,
+    };
+  },
+  computed: {
+    sumaPresupost() {
+      this.total = 0;
+      for (let item of this.selected) {
+        this.total = this.total + item;
+      }
+      return this.total;
+    },
+  },
 };
 </script>
