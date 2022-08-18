f08d99cde599b10b3c08fd7b3eccc452d6dd7a75
Xavier Mas <xvrmasgarcia@gmail.com>
2022-08-18 19:28:57 +0200

    emits exercici2

---
+++ src/components/Panell.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="carta">
+      Número de paginas <input id="imp1" type="number" /><br /><br />
+      Número de idiomas <input id="imp2" type="number" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Panell",
+  props: ["selected"],
+  total: 0,
+  computed: {
+    sumaPresupost() {
+      this.total = 0;
+      for (let item of this.selected) {
+        this.total = this.total + item;
+      }
+      return this.total;
+    },
+  },
+  watch: {
+    sumaPresupost: function () {
+      this.$emit("resultat", this.sumaPresupost);
+    },
+  },
+};
+</script>
+<style scoped>
+.carta {
+  padding: 1rem;
+  height: auto;
+  width:fit-content;
+  border: solid 3px;
+  border-radius: 15px;
+  margin: 1%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="home ml-5">
-    <h4>¿Que quieres hacer?</h4>
+    <h4>¿Qué quieres hacer?</h4>
     <div class="m-3">
-      <b-form-checkbox-group
-        stacked
-        v-model="selected"
-        :options="options"
-        value-field="preu"
-        text-field="name"
-      ></b-form-checkbox-group>
-      <h4 class="m-3">Preu: {{ sumaPresupost }} €</h4>
+      <b-form-checkbox id="check1" v-model="selected" :value="500"
+        >Una pàgina web (500 €)</b-form-checkbox
+      >
+      <panell :selected="selected" @resultat="totalSuma"></panell>
+
+      <b-form-checkbox id="check2" v-model="selected" :value="300"
+        >Una consultoria SEO (300 €)</b-form-checkbox
+      >
+      <b-form-checkbox id="check3" v-model="selected" :value="200"
+        >Una campanya de Google Ads (200 €)</b-form-checkbox
+      >
+      <div>
+        <h5>Preu:{{ resultat }} €</h5>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Panell from "@/components/Panell.vue";
 export default {
+  components: { Panell },
   name: "HomeView",
   data() {
     return {
       selected: [],
-      options: [
-        { preu: 500, name: "Una pàgina web (500 €)" },
-        { preu: 300, name: "Una consultoria SEO (300€)" },
-        { preu: 200, name: "Una campanya de Google Ads (200 €)" },
-      ],
-      total: 0,
+      resultat: 0,
     };
   },
-  computed: {
-    sumaPresupost() {
-      this.total = 0;
-      for (let item of this.selected) {
-        this.total = this.total + item;
-      }
-      return this.total;
+  methods: {
+    totalSuma(value) {
+      this.resultat = value;
     },
   },
 };
